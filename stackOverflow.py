418da1d6c1dd017fa88235aa844116864f1e5a07
Daniel Dao <daniel.vu.dao@gmail.com>
2015-10-12 21:13:54 -0500

    wip

--- stackOverflow.py
+++ stackOverflow.py
@@ -2,11 +2,26 @@ import requests
 import json
 
 
-url = "https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow"
+request_url = "https://api.stackexchange.com/2.2/questions?order=desc&pagesize=100&sort=activity&tagged=python&site=stackoverflow"
 
 
-response = requests.get(url)
-data = json.loads(response.content)
+response = requests.get(request_url)
+question_data = response.json()
 
-with open('output.json', 'w') as outfile:
-	json.dump(data, outfile)
+# print question_data['items']
+ids = []
+
+for question in question_data['items']:
+	# _ids.append(str(question['question_id'])
+	# ids.append(str(question['question_id']))
+	# if question['is_answered'] == True:
+	answer_url = 'https://api.stackexchange.com/2.2/questions/' + str(question['question_id']) + '/answers?order=desc&sort=activity&site=stackoverflow&filter=withbody'
+	answer_response = requests.get(answer_url).json()
+	with open('output.json', 'w') as outfile:
+		json.dump(answer_response, outfile)
+	
+
+
+# ';'.join(ids)
+
+
