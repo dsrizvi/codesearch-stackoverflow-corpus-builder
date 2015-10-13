3bdfdd2f51e2b7ac028f9cb9cd43e927b2356419
Danyal <d.s.rizvi@gmail.com>
2015-10-13 16:44:10 -0500

    beautiful soup added

--- stackOverflow.py
+++ stackOverflow.py
@@ -1,8 +1,10 @@
-import requests 
+import requests
 import json
 import time
+from BeautifulSoup import BeautifulSoup
 
-request_url = "https://api.stackexchange.com/2.2/questions?order=desc&pagesize=5&sort=votes&min=20&tagged=python&site=stackoverflow"
+
+request_url = "https://api.stackexchange.com/2.2/questions?order=desc&pagesize=5&sort=votes&min=20&tagged=python&site=stackoverflow&filter=withbody"
 
 
 response = requests.get(request_url)
@@ -13,16 +15,30 @@ ids = []
 
 for question in question_data["items"]:
 	if question['is_answered'] == True:
-		print question["title"]
 		answer_url = 'https://api.stackexchange.com/2.2/questions/' + str(question['question_id']) + '/answers?order=desc&sort=activity&site=stackoverflow&filter=withbody'
-		time.sleep(31)
+		time.sleep(1)
 		answer_response = requests.get(answer_url).json()
 		for answer in answer_response["items"]:
-			if 'body' in answer:
-				print answer['body']
-			
-		
+			if answer['is_accepted']:
+				# print question["title"].encode('utf-8')
+				# print '-----------------------------------'
+				# print question['body'].encode('utf-8')
+				# print '-----------------------------------'
+				# print answer['body'].encode('utf-8')
+				# print '==================================='
+				question 	  = question["title"].encode('utf-8')
+				print question
+				if 'body' in question:
+					question_body = question['body'].encode('utf-8')
 
+				answer_body	  = answer['body'].encode('utf-8')
+				soup 		  = BeautifulSoup(answer_body)
+
+				code_snippets = soup.find('code').getText()
+
+				print code_snippets
+				print '==================================='
+				break
 
 # ';'.join(ids)
 
