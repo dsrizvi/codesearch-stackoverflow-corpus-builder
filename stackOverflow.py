863748272ce195321cf1ff7a099216c87e8de733
Akshay <akshaykasukhela@utexas.edu>
2015-10-12 21:44:22 -0500

    almost done with scrapper

--- stackOverflow.py
+++ stackOverflow.py
@@ -1,8 +1,8 @@
 import requests 
 import json
+import time
 
-
-request_url = "https://api.stackexchange.com/2.2/questions?order=desc&pagesize=100&sort=activity&tagged=python&site=stackoverflow"
+request_url = "https://api.stackexchange.com/2.2/questions?order=desc&pagesize=5&sort=votes&min=20&tagged=python&site=stackoverflow"
 
 
 response = requests.get(request_url)
@@ -11,15 +11,17 @@ question_data = response.json()
 # print question_data['items']
 ids = []
 
-for question in question_data['items']:
-	# _ids.append(str(question['question_id'])
-	# ids.append(str(question['question_id']))
-	# if question['is_answered'] == True:
-	answer_url = 'https://api.stackexchange.com/2.2/questions/' + str(question['question_id']) + '/answers?order=desc&sort=activity&site=stackoverflow&filter=withbody'
-	answer_response = requests.get(answer_url).json()
-	with open('output.json', 'w') as outfile:
-		json.dump(answer_response, outfile)
-	
+for question in question_data["items"]:
+	if question['is_answered'] == True:
+		print question["title"]
+		answer_url = 'https://api.stackexchange.com/2.2/questions/' + str(question['question_id']) + '/answers?order=desc&sort=activity&site=stackoverflow&filter=withbody'
+		time.sleep(31)
+		answer_response = requests.get(answer_url).json()
+		for answer in answer_response["items"]:
+			if 'body' in answer:
+				print answer['body']
+			
+		
 
 
 # ';'.join(ids)
