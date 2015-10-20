50fd11089da3d59601c38ed2aef286db36504a54
Danyal <d.s.rizvi@gmail.com>
2015-10-20 17:50:52 -0500

    working on removing code blacks via regex

--- stackOverflow.py
+++ stackOverflow.py
@@ -2,6 +2,8 @@ import requests
 import json
 import time
 from BeautifulSoup import BeautifulSoup
+import html2text
+import re
 
 
 request_url = "https://api.stackexchange.com/2.2/questions?order=desc&pagesize=5&sort=votes&min=20&tagged=python&site=stackoverflow&filter=withbody"
@@ -33,12 +35,30 @@ for question in question_data["items"]:
 
 				answer_body	  = answer['body'].encode('utf-8')
 				soup 		  = BeautifulSoup(answer_body)
-
 				code_snippets = soup.find('code').getText()
+				# print answer_body
+				# print soup.getText()
+				qid = answer['question_id']
 
-				print code_snippets
+				# for i in range(len(soup.find('code'))):
+					# cid = 'so' + str(i) + '_' + str(qid)
+
+				cid = '------------------'
+				answer_body = re.sub(r'<code>(.+?)</code>', cid, answer_body)
+
+
+
+				# answer_body = re.sub(r'<code>.*</code>', '', answer_body)
+				answer_body = html2text.html2text(answer_body).replace('\n', '<br>')
+				print answer['body'].encode('utf-8')
+				print '-----------------------------------'
+				print answer_body
+
+				# print answer_body
+
+				# print code_snippets
 				print '==================================='
-				break
+
 
 # ';'.join(ids)
 
