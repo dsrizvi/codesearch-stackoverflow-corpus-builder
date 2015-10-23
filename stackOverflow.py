c653f9dd1dd82e9ec548bea75e97b05b27d5b8c3
Danyal <d.s.rizvi@gmail.com>
2015-10-22 23:40:26 -0500

    redid all the code

--- stackOverflow.py
+++ stackOverflow.py
@@ -4,56 +4,146 @@ import time
 from BeautifulSoup import BeautifulSoup
 import html2text
 import re
+import sqlite3
+import json
+# client_id 		= '5836'
+# client_secret 	= 'AN8VH9S*GiY9j2MpgfE8jw(('
+# api_key			= ')HzqRSuw*14xiB8Yc8cgZw(('
+# questions_url	= "https://api.stackexchange.com/2.2/questions?key=KEY&page=PAGEorder=desc&pagesize=100&sort=votes&min=20&tagged=python&site=stackoverflow&filter=withbody&page=PAGENUM"
+# answer_url 		= "https://api.stackexchange.com/2.2/questions/QUESTIONID/answers?order=desc&page=sort=activity&site=stackoverflow&filter=withbody"
+# https://api.stackexchange.com/2.0/questions?key=)HzqRSuw*14xiB8Yc8cgZw((&pagesize=50&site=stackoverflow&tagged=xpages&order=desc&sort=creation&page=1
 
 
-request_url = "https://api.stackexchange.com/2.2/questions?order=desc&pagesize=5&sort=votes&min=20&tagged=python&site=stackoverflow&filter=withbody"
+def main():
+
+	client_id 			= '5836'
+	client_secret 		= 'AN8VH9S*GiY9j2MpgfE8jw(('
+	api_key				= ')HzqRSuw*14xiB8Yc8cgZw(('
+	questions_url		= "https://api.stackexchange.com/2.2/questions?key={key}&page={page}&order=desc&pagesize=100&sort=votes&min=20&tagged=python&site=stackoverflow&filter=withbody"
+	answer_url 			= "https://api.stackexchange.com/2.2/questions/{question_id}/answers?order=desc&sort=activity&site=stackoverflow&filter=withbody&key=PLACEHOLDER"
+	answer_url 			= answer_url.replace("PLACEHOLDER", api_key)
+
+	page 		   		= 1
+	requests_remaining  = 10
+	questions_url  		= questions_url.format(key=api_key, page=1)
 
 
-response = requests.get(request_url)
-question_data = response.json()
+	while requests_remaining > 0:
 
-# print question_data['items']
-ids = []
+		questions, requests_remaining	= get_questions(url=questions_url, page=page)
+		page 		  	   				+= 1
 
-for question in question_data["items"]:
-	if question['is_answered'] == True:
-		answer_url = 'https://api.stackexchange.com/2.2/questions/' + str(question['question_id']) + '/answers?order=desc&sort=activity&site=stackoverflow&filter=withbody'
-		time.sleep(1)
-		answer_response = requests.get(answer_url).json()
-		for answer in answer_response["items"]:
-			if answer['is_accepted']:
-				# print question["title"].encode('utf-8')
-				# print '-----------------------------------'
-				# print question['body'].encode('utf-8')
-				# print '-----------------------------------'
-				# print answer['body'].encode('utf-8')
-				# print '==================================='
-				question 	  = question["title"].encode('utf-8')
-				print question
-				if 'body' in question:
-					question_body = question['body'].encode('utf-8')
+		# print questions
+		if questions:
+			answers, requests_remaining  = build_qa(questions=questions,url=answer_url, requests_remaining=requests_remaining)
 
-				answer_body	  = answer['body'].encode('utf-8')
-				soup 		  = BeautifulSoup(answer_body)
-				code_snippets = soup.findAll('code')
-				# print answer_body
-				# print soup.getText()
-				qid = answer['question_id']
-				print len(soup.findAll('code'))
-				print "++++++++++++++++++"
-				for i in range(len(soup.findAll('code'))):
-					cid = 'so' + str(i) + '_' + str(qid) + '.txt'
-					answer_body = re.sub(r'<code>(.|\n)*?<\/code>', cid, answer_body, 1)
-					code_file = open(cid, "w")
-					code_file.write(str(code_snippets[i]))
-					code_file.close()
+		time.sleep(30)
 
-				answer_body = html2text.html2text(answer_body)
-				print answer_body
+def get_questions(url, page):
 
-				print '==================================='
+	url = url.format(page=page)
+	response = requests.get(url)
+
+	print "Fetching questions..."
+
+	if 'error_id' in response:
+		print "ERROR"
+		if response['error_id'] == 502:
+			print response
+			wait_time = re.findall(r'\d+', response['error_message'])[0]
+			print "API limit reached. Sleeping for " + str(wait_time) + 'seconds'
+			time.sleep(float(wait_time))
+			print "Resuming..."
+	else:
+		print "Questions fetched."
+		questions = response.json()
+		return questions['items'], questions['quota_remaining']
 
 
-# ';'.join(ids)
+def build_qa(url, questions, requests_remaining):
+
+	print "Building QA..."
+
+	qas = []
+
+	for question in questions:
+		if question['is_answered']:
+			answers_url			= url.format(question_id=str(question['question_id']))
+			# answer_url 	   		= url.replace("QUESTIONID", str(question['question_id']))
+			requests_remaining -= 1
+			# print answers_url
+			response = requests.get(answers_url).json()
+			print response
+			if 'error_id' in response:
+				print "ERROR"
+				print response
+				if response['error_id'] == 502:
+					wait_time = re.findall(r'\d+', response['error_message'])[0]
+					print "API limit reached. Sleeping for " + str(wait_time) + 'seconds'
+					time.sleep(float(wait_time))
+					answers = requests.get(answer_url).json()
+
+			if 'error_id' not in response:
+				for answer in response['items']:
+					if 'error_id' in answer:
+						answer = None
+						print "ERROR:"
+						print answers
+						print 'Question id:' + question['id'].encode('utf-8')
+						break
+					else:
+						if answer['is_accepted']:
+							answer_body  = answer['body'].encode('utf-8')
 
 
+							question_id    = answer['question_id']
+							question_title = question['title'].encode('utf-8')
+							question_body  = question['body'].encode('utf-8')
+							question_link  = question['link'].encode('utf-8')
+
+							soup 		  = BeautifulSoup(answer_body)
+							code_extract  = soup.findAll('code')
+							code_snippets = {}
+
+
+							print answer_body
+							for i in range(len(code_extract)):
+								cid 		       = 'so_' + str(i) + '_' + str(question_id) + '.code'
+								code 		       = re.search(r'(?<=<code>)(.|\n)*?(?=\<\/code>)', answer_body)
+								# code 		       = re.search(r'<code>(.|\n)*?<\/code>', answer_body)
+								answer_body 	   = re.sub(r'<code>(.|\n)*?<\/code>', cid, answer_body, 1)
+
+								code_snippets[cid] = code.group(0)
+
+							answer_body = html2text.html2text(answer_body.decode('utf-8'))
+							answer_body = answer_body.replace('\n', '<br>')
+							qa 			= { 'qid'  		   : question_id,
+									   		'question_body' : question_body,
+									   		'question_body' : question_title,
+								   	   		'question_link' : answers_url,
+								      		 'answer_body'   : answer_body,
+								      		 'code_snippets' : code_snippets
+										}
+							qas.append(answer)
+							# print answer
+							break
+			time.sleep(1)
+
+	print "Building QA complete"
+	return qa, requests_remaining
+
+def build_html(qas):
+
+	for qa in qas:
+
+		qa = { 	   'qid'  		   : question_id,
+				   'question_body' : question_body,
+				   'question_body' : question_title,
+			   	   'question_link' : answers_url,
+			       'answer_body'   : answer_body,
+			       'code_snippets' : code_snippets
+		}
+
+
+if __name__ == '__main__':
+	main()
