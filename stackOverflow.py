e6ef91a414f80655dd807a1908033e51bd40fe81
Danyal <d.s.rizvi@gmail.com>
2015-10-23 04:48:17 -0500

    final heroku attempt for the night

--- stackOverflow.py
+++ stackOverflow.py
@@ -57,7 +57,8 @@ def index():
 	# db_port				= 5432
 	# db_password			= 'socrawler'
 	print '0'
-	conn 				= psycopg2.connect(dbname=db_name, user=db_user, password=db_password, port=db_port, host=db_host)
+	conn 				= psycopg2.connect(database=db_name, user=db_user, password=db_password,
+										   port=db_port, host=db_host)
 	print conn
 	print "1"
 	page 		   		= 1
