c56bf8ffdba6a9d761622118c0937f7cc10a2983
Danyal <d.s.rizvi@gmail.com>
2015-10-27 19:45:34 -0500

    conf error fixed

--- stackOverflow.py
+++ stackOverflow.py
@@ -27,7 +27,7 @@ app = Flask(__name__)
 # app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 # app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+app.config.update(BROKER_URL=os.environ['REDIS_URL'],
                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 
