7f9d703c12c4655c93ee3bd7da19110254d68cdf
Danyal <d.s.rizvi@gmail.com>
2015-11-03 22:06:04 -0600

    indentation error fixed

--- stackOverflow.py
+++ stackOverflow.py
@@ -74,7 +74,7 @@ def get_pagelog(bucket, name, folder, curr_page):
 		try:
 			print 'log does not exist! creating...'
 			pagelog = [(datetime.now(), curr_page)]
-			page = pickle.dump(pagelog)]
+			page = pickle.dump[(pagelog)]
 			print 'uploading %s' %pagelog
 			s3upload(pagelog_name, page, bucket)
 		except Exception as e:
