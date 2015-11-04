23f14868724c5b33d3ae5b61a6092d1be638946a
Danyal <d.s.rizvi@gmail.com>
2015-11-03 22:16:25 -0600

    im a fucking idiot

--- stackOverflow.py
+++ stackOverflow.py
@@ -73,7 +73,7 @@ def get_pagelog(bucket, name, folder, curr_page):
 		try:
 			print 'log does not exist! creating...'
 			pagelog = [(datetime.now(), curr_page)]
-			page = pickle.dump(pagelog)
+			page = pickle.dumps(pagelog)
 			print 'uploading %s' %pagelog
 			s3upload(name=pagelog_name, doc=page, bucket=bucket, folder=folder)
 		except Exception as e:
